409748ed9d98d067132b61551e1e892c1e4ccbf3
Omar Shaikh <oshaikh13@gmail.com>
2019-04-07 16:51:51 -0400

    use env

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ Promise = require('bluebird'); // eslint-disable-line no-global-assign
 mongoose.Promise = Promise;
 
 // connect to mongo db
-mongoose.connect(config.db, { server: { socketOptions: { keepAlive: 1 } } });
+mongoose.connect(process.env.MONGODB_URI || config.db, { server: { socketOptions: { keepAlive: 1 } } });
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${config.db}`);
 });
@@ -28,7 +28,7 @@ if (config.MONGOOSE_DEBUG) {
 // src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
   // listen on port config.port
-  app.listen(config.port, () => {
+  app.listen(process.env.PORT || config.port, () => {
     debug(`server started on port ${config.port} (${config.env})`);
   });
 }
